efb86b8b02956b5698d8864e182510e0c5f3c9d1
Gabriele Guastella <gabrio.guastella@gmail.com>
2025-10-22 23:56:24 +0200

    Added ScriptableObjects, Grid position and interaction
    I added all the different chess pieces as scriptable object in order to have a nicer environment to work with in the future. I also added and set up the grid even if it still not perfect.

---
+++ Chess-o/Assets/Scripts/ChessPieces.cs
@@ -0,0 +1,15 @@
+using System.IO.Enumeration;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ChessPiece", menuName = "ScriptableObjects/ChessPiece" )]
+public class ChessPieces : ScriptableObject
+{   
+   
+    public Sprite Sprite;
+    public int value;
+    public bool isGreen;
+    public PieceType type;
+}
+
+public enum PieceType { King, Queen, Rook, Bishop, Knight, Pawn }
+
---
+++ Chess-o/Assets/Scripts/Piece.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Piece : MonoBehaviour, IPointerClickHandler
+{
+    public ChessPieces data;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = data.Sprite;
+    }
+
+    public void MovePieceToGrid()
+    {
+
+    }
+
+    void ShowPossiblePos()
+    {
+        switch (data.type)
+        {
+            case PieceType.King:
+
+
+
+                break;
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        gameObject.gameObject.SetActive(false);
+    }
+}
